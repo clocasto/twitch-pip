34d03c7e3b4cfe3c0f7f43ee9a036b6f2165703f
Chris LoCasto <locasto1@gmail.com>
2016-09-24 12:06:04 -0500

    div swapping functional

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,37 +1,55 @@
 import React, {Component} from 'react'
 import Console from './Console'
+import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 
 class Player extends Component {
   render() {
+
 		let { style } = this.props
 		let div1Style = {
-			"backgroundColor": style.thin.color,
-			height: style.thin.height,
-			width: style.thin.width,
+			backgroundColor: style.thin.color,
+			height: style[style.one].height,
+			width: style[style.one].width,
 			position: style.thin.position,
 			top: style[style.one].top,
-			left: style[style.one].left
+			left: style[style.one].left,
+			bottom: style[style.one].bottom,
+			right: style[style.one].right,
+			zIndex: style[style.one].zIndex
 		}
 		console.log(style[style.two].color)
 		let div2Style = {
-			"backgroundColor": style.fat.color,
-			height: style.fat.height,
-			width: style.fat.width,
+			backgroundColor: style.fat.color,
+			height: style[style.two].height,
+			width: style[style.two].width,
 			position: style.fat.position,
 			bottom: style[style.two].bottom,
-			right: style[style.two].right
+			right: style[style.two].right,
+			top: style[style.two].top,
+			left: style[style.two].left,
+			zIndex: style[style.two].zIndex
 		}
+		let pdiv = {
+			position: 'relative',
+			height: '400px'
+		}
+
     return (
-    <div>
+    	<div style={pdiv}>
+				<div style={div2Style}>
+				
+				</div>
+			<br />
+			<br />
+			<br />
+			<br />
+
 			<div style={div1Style}>
 			</div>
+
 			<br />
 			<br />
-			<br />
-			<br />
-			<div style={div2Style}>
-			</div>
-			<h1>I am the player div!</h1>
+
 			<Console {...this.props}/>
 		</div>
 		);
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,22 +5,39 @@ import { browserHistory } from 'react-router';
 
 import rootReducer from './reducers/reducers.js';
 
+const fat = {
+    height: '100%',
+    width: '100%',
+    color: 'red',
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    top: null,
+    left: null,
+    zIndex: -1
+}
+
+const thin = {
+    height: 100,
+    width: 200,
+    color: 'green',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: null,
+    right: null,
+    zIndex: 1
+}
+
+
 const defaultState = {
-   playerReducer: {alex: 'memes'},
-   buttonReducer: {thin: {height: 100,
-                   width: 200,
-                   color: 'green',
-                   position: 'absolute',
-                   top: 0,
-                   left: 0},
-                   fat: {height: 100,
-                         width: 400,
-                         color: 'red',
-                         position: 'absolute',
-                         right: 0,
-                         bottom: 0},
-                    one: 'thin',
-                    two: 'fat'}
+    playerReducer: { alex: 'memes' },
+    buttonReducer: {
+        thin: thin,
+        fat: fat,
+        one: 'thin',
+        two: 'fat'
+    }
 }
 
 const store = createStore(rootReducer, defaultState);
@@ -28,10 +45,10 @@ const store = createStore(rootReducer, defaultState);
 export const history = syncHistoryWithStore(browserHistory, store);
 
 // if (module.hot) {
-// 	module.hot.accept('./reducers/', () => {
-// 		let nextRootReducer = require('./reducers/reducers').default;
-// 		store.replaceReducer(nextRootReducer);
-// 	})
+//  module.hot.accept('./reducers/', () => {
+//    let nextRootReducer = require('./reducers/reducers').default;
+//    store.replaceReducer(nextRootReducer);
+//  })
 // }
 
 export default store;
