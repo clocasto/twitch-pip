a2f86181b03222e2714f94c1198c0cb4c3724787
Alexei Bazhenov <abazhenov@gmail.com>
2016-09-25 15:36:54 -0500

    colums working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import Player from './player.jsx';
-import ControlPanel from './navbar.jsx'
+import Navigation from './navbar.jsx'
 
 class Main extends Component {
   render() {
     return (
-      <div className="App">
-      	<ControlPanel />
-        <Player {...this.props} />
+      <div className="App row">
+      	<Navigation />
+        <div className="col-md-9">
+          <Player {...this.props} />
+        </div>
+        <div className="col-md-3">
+
+        </div>
       </div>
     );
   }
